0dbf9a3de8ad878807e0a28be6c6c35022a7ec1c
李宏利 <lihongli@eastfair.com>
2019-12-02 18:27:27 +0800

    定时任务 Quartz.Net

---
+++ Quartz.Net.Framework/HelloJob.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*----------------------------------------------------------------
+* 项目名称 ：Quartz.Net.Framework
+* 项目描述 ：
+* 类 名 称 ：HelloJob
+* 类 描 述 ： 
+* 作    者 ：lihongli
+* 创建时间 ：2019/12/2 17:24:41
+* 版 本 号 ：v1.0.0.0
+----------------------------------------------------------------*/
+namespace Quartz.Net.Framework
+{
+    [PersistJobDataAfterExecution]
+    [DisallowConcurrentExecution]
+    public class HelloJob : IJob
+    {
+
+        public string Name { get; set; }
+
+        public int Times { get; set; }
+
+
+        /// <summary>
+        /// 作业调度定时执行的方法
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public async Task Execute(IJobExecutionContext context)
+        {
+            //    context.Scheduler 调度器信息
+            //    context.Trigger 触发器信息
+            //    context.JobDetail 作业信息
+            //    context.ScheduledFireTimeUtc 当前触发时间
+            //    context.NextFireTimeUtc 下一次将被触发时间
+
+            context.JobDetail.JobDataMap["times"] = Times + 1;
+
+            await Console.Out.WriteLineAsync(
+               $"{context.JobDetail.Key}--》{context.NextFireTimeUtc.ToString()}-----{Name}----{Times}"  );
+
+        }
+    }
+}
---
+++ Quartz.Net.Framework/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz.Impl;
+
+namespace Quartz.Net.Framework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunScheduler();
+
+            Console.ReadKey();
+        }
+
+
+
+        private static async Task RunScheduler()
+        {
+            // 创建作业调度池
+            ISchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+
+
+            var jobDataMap = new JobDataMap();
+            jobDataMap.Add("name", "beck");
+            jobDataMap.Add("times", 1);
+
+            // 创建作业
+            IJobDetail job = JobBuilder.Create<HelloJob>()
+                .WithIdentity("job1", "group1")
+                .UsingJobData(jobDataMap)
+                .Build();
+
+            // 创建触发器，每10s执行一次
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(10)
+                    .RepeatForever())
+                .WithCronSchedule("0 0 8 * * ?") //每天早上8点
+                .Build();
+
+            // 加入到作业调度池中
+            await scheduler.ScheduleJob(job, trigger);
+
+            // 开始运行
+            await scheduler.Start();
+        }
+    }
+}
